c35c0b8ec958dbdf40ce9c2f5709fad0d07e013a
a.amomar <amomar@thiqah.sa>
2019-02-09 13:06:24 +0300

    Read-WriteToexcel

---
+++ junitCucWithCommon/src/test/java/common/commonExcelAction.java
@@ -0,0 +1,38 @@
+package common;
+
+import java.io.IOException;
+
+import resources.ReadExcelFile;
+import resources.WriteExcelFile;
+
+public class commonExcelAction {
+	
+	public void readTheExcelFile() throws IOException {
+		
+		ReadExcelFile objExcelFile =new ReadExcelFile();
+		 
+		  //Prepare the path of excel file
+
+	    String filePath = System.getProperty("user.dir")+".\\files";
+
+	    //Call read file method of the class to read data
+
+	    objExcelFile.readExcel(filePath,"data.xlsx","users");
+	}
+	
+	public void writeTheExcelFile() throws IOException {
+		 //Create an array with the data in the same order in which you expect to be filled in excel file
+
+        String[] valueToWrite = {"Mr. E","Noida","name"};
+
+        //Create an object of current class
+
+        WriteExcelFile objExcelFile = new WriteExcelFile();
+
+        //Write the file using file name, sheet name and the data to be filled
+
+        objExcelFile.writeExcel(System.getProperty("user.dir")+".\\files","data.xlsx","users",valueToWrite);
+		  
+	}
+
+}
